b34515d05c845730d30fd25b3275f189b7143981
Johan Edstedt <johed950@student.liu.se>
2021-02-03 17:37:49 +0100

    add modified emd_loss which doesnt saturate.

--- mmaction/models/losses/emd_loss.py
+++ mmaction/models/losses/emd_loss.py
@@ -23,26 +23,32 @@ class EMDLoss(BaseWeightedLoss):
         Returns:
                 torch.Tensor: Mean forward KL-divergence over the batch .
         """
-        q = F.softplus(cls_score)
-        loss = EMDLoss.emd_1d(q,p*q.detach().sum(-1)[:,None],self.C[None,...].expand(len(q),-1,-1))
+        #q = cls_score.softmax()#F.softplus(cls_score)
+        loss = EMDLoss.emd_1d(cls_score,p,self.C[None,...].expand(len(cls_score),-1,-1))
         return loss
 
     @staticmethod
-    def emd_1d(a,b,C):
+    def emd_1d(e,b,C):
         """
         Calculate EMD under the assumption that the distributions a,b are sorted and that the cost function behaves nicely.
         C is the associated cost matrix.
         If a is of shape (b,n), then the function is reduced over the batched dimension (mean reduction)
         """
-        if a.dim() > 1:
-            return sum(EMDLoss.emd_1d(a[k],b[k],C[k]) for k in range(len(a)))/len(a)
+
+        if e.dim() > 1:
+            return sum(EMDLoss.emd_1d(e[k],b[k],C[k]) for k in range(len(e)))/len(e)
+        a = e.softmax(dim=-1)
+        log_a = e.log_softmax(dim=-1)
+
+
         N,M,zero = len(a),len(b), torch.zeros(1,device=a.device)
         F,G = torch.cumsum(torch.cat((zero,a)),0),torch.cumsum(torch.cat((zero,b)),0)
         j,cost = 0,0.
+        #print(log_a)
         for i in range(N):
             for j in range(j,M):
                 delta = min(F[i+1],G[j+1])-max(F[i],G[j])
-                cost += delta*C[i,j]
+                cost += delta*C[i,j]*(-log_a[j])
                 if G[j+1] >= F[i+1]: break
         return cost
 
