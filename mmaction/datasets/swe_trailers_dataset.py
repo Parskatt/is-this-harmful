01f30ff04a177bc3fe43e4207f88e6ff16d529be
Johan Edstedt <johan.edstedt@liu.se>
2021-04-19 09:58:41 +0200

    added class balanced sampling option

--- mmaction/datasets/swe_trailers_dataset.py
+++ mmaction/datasets/swe_trailers_dataset.py
@@ -2,6 +2,7 @@ import copy
 import os.path as osp
 import json
 from os.path import join
+from collections import defaultdict
 
 import mmcv
 import numpy as np
@@ -11,9 +12,9 @@ from ..core import mean_average_precision
 from .base import BaseDataset
 from .registry import DATASETS
 
-from ..core import (mean_class_accuracy,top_k_accuracy,confusion_matrix,wasserstein_1_distance,KL,euclidean_distance)
-
-
+from ..core import (mean_class_accuracy, top_k_accuracy, confusion_matrix,
+                    wasserstein_1_distance, KL, euclidean_distance, class_euclidean_distance)
+import random
 
 @DATASETS.register_module()
 class SweTrailersDataset(BaseDataset):
@@ -29,18 +30,21 @@ class SweTrailersDataset(BaseDataset):
             Default: False.
         label_as_distribution (bool): if the label should be converted into a distribution 
     """
-    def __init__(self, ann_file, pipeline,data_prefix=None, num_classes = 4,label_as_distribution=True, **kwargs):
+
+    def __init__(self, ann_file, pipeline, data_prefix=None, num_classes=4, label_as_distribution=True, sample_by_class=False, **kwargs):
         self.label_as_distribution = label_as_distribution
         self.data_prefix = data_prefix
-        self._label_to_ind = {"bt":0,"7":1,"11":2,"15":3}
-        super().__init__(ann_file, pipeline, num_classes=num_classes,data_prefix=data_prefix, **kwargs)
-        
-    def label_to_ind(self,lbl):
+        self.num_classes = 4
+        self._label_to_ind = {"bt": 0, "7": 1, "11": 2, "15": 3}
+        super().__init__(ann_file, pipeline, num_classes=num_classes,
+                         data_prefix=data_prefix, sample_by_class=sample_by_class, **kwargs)
+
+    def label_to_ind(self, lbl):
         return self._label_to_ind[lbl]
 
     def load_annotations(self):
         """Load annotation file to get video information."""
-        video_infos = json.load(open(self.ann_file,"r"))
+        video_infos = json.load(open(self.ann_file, "r"))
         for clip in video_infos:
             clip["orig_label"] = clip["label"].copy()
             if self.label_as_distribution:
@@ -53,16 +57,29 @@ class SweTrailersDataset(BaseDataset):
             else:
                 lbl = clip["label"][0]
                 clip["label"] = self.label_to_ind(lbl)
-            clip["audio_path"] = join(self.data_prefix,clip["filename"],clip["filename"]+".npy")
+            clip["audio_path"] = join(
+                self.data_prefix, clip["filename"], clip["filename"]+".npy")
         return video_infos
-    
+
+    def parse_by_class(self):
+        video_infos_by_class = defaultdict(list)
+        for item in self.video_infos:
+            label = item['orig_label']
+            for lbl in label:
+                video_infos_by_class[self._label_to_ind[lbl]].append(item)
+        return video_infos_by_class
+
     def prepare_train_frames(self, idx):
         """Prepare the frames for training given the index."""
-        results = copy.deepcopy(self.video_infos[idx])
+        if self.sample_by_class:
+            results = random.choice(random.choice(self.video_infos_by_class))
+        else:
+            results = self.video_infos[idx]
+        results = copy.deepcopy(results)
         results['modality'] = self.modality
         results['start_index'] = self.start_index
         results['filename_tmpl'] = results["filename"]+'_{}.jpg'
-        results['frame_dir'] = join(self.data_prefix,results["filename"])
+        results['frame_dir'] = join(self.data_prefix, results["filename"])
         return self.pipeline(results)
 
     def prepare_test_frames(self, idx):
@@ -71,15 +88,15 @@ class SweTrailersDataset(BaseDataset):
         results['modality'] = self.modality
         results['start_index'] = self.start_index
         results['filename_tmpl'] = results["filename"]+'_{}.jpg'
-        results['frame_dir'] = join(self.data_prefix,results["filename"])
+        results['frame_dir'] = join(self.data_prefix, results["filename"])
         return self.pipeline(results)
 
     def evaluate(self,
-                results,
-                metrics='confusion_matrix',
-                metric_options=dict(top_k_accuracy=dict(topk=(1,))),
-                logger=None,
-                **deprecated_kwargs):
+                 results,
+                 metrics='confusion_matrix',
+                 metric_options=dict(top_k_accuracy=dict(topk=(1,))),
+                 logger=None,
+                 **deprecated_kwargs):
         """Perform evaluation for common datasets.
         Args:
             results (list): Output results.
@@ -115,7 +132,7 @@ class SweTrailersDataset(BaseDataset):
 
         metrics = metrics if isinstance(metrics, (list, tuple)) else [metrics]
         allowed_metrics = [
-            'top_k_accuracy', 'confusion_matrix', 'mean_class_accuracy','wasserstein','KL','euclidean'
+            'top_k_accuracy', 'confusion_matrix', 'mean_class_accuracy', 'wasserstein', 'KL', 'euclidean', 'class_euclidean'
         ]
 
         for metric in metrics:
@@ -123,17 +140,17 @@ class SweTrailersDataset(BaseDataset):
                 raise KeyError(f'metric {metric} is not supported')
 
         eval_results = {}
-        #We will save a log file with all the outputs, we assume that the results are ordered
+        # We will save a log file with all the outputs, we assume that the results are ordered
         if not self.label_as_distribution:
             gt_labels = [ann['label'] for ann in self.video_infos]
         else:
             gt_labels = [np.argmax(ann['label']) for ann in self.video_infos]
         gt_filenames = [ann['filename'] for ann in self.video_infos]
         log_msg = ""
-        for pred,ann in zip(results,self.video_infos):
+        for pred, ann in zip(results, self.video_infos):
             log_msg += f"Clip: {ann['filename']}\n\t Pred: {pred} \n\t GT: {ann['label']}\n"
         print_log(log_msg, logger=logger)
-        
+
         for metric in metrics:
             msg = f'Evaluating {metric} ...'
             if logger is None:
@@ -142,14 +159,14 @@ class SweTrailersDataset(BaseDataset):
 
             if metric == 'top_k_accuracy':
                 topk = metric_options.setdefault('top_k_accuracy',
-                                                    {}).setdefault('topk', (1, 5))
+                                                 {}).setdefault('topk', (1, 5))
                 if not isinstance(topk, (int, tuple)):
                     raise TypeError('topk must be int or tuple of int, '
                                     f'but got {type(topk)}')
                 if isinstance(topk, int):
                     topk = (topk, )
 
-                top_k_acc = top_k_accuracy(results, gt_labels, (1,2))
+                top_k_acc = top_k_accuracy(results, gt_labels, (1, 2))
                 log_msg = []
                 for k, acc in zip(topk, top_k_acc):
                     eval_results[f'top{k}_acc'] = acc
@@ -160,49 +177,68 @@ class SweTrailersDataset(BaseDataset):
                 mean_acc = mean_class_accuracy(results, gt_labels)
                 eval_results['mean_class_accuracy'] = mean_acc
                 log_msg = f'\nMean class accuracy: \n {mean_acc:.4f}'
-                print_log(log_msg, logger=logger)            
+                print_log(log_msg, logger=logger)
             elif metric == 'confusion_matrix':
-                result_argmax = np.argmax(results,axis=1)
-                confusion_mat = confusion_matrix(result_argmax,gt_labels)
+                result_argmax = np.argmax(results, axis=1)
+                confusion_mat = confusion_matrix(result_argmax, gt_labels)
                 eval_results['confusion_matrix'] = confusion_mat
                 log_msg = f'\nConfusion Matrix:\n{confusion_mat}'
                 print_log(log_msg, logger=logger)
             elif metric == 'wasserstein':
-                gt_distributions = np.zeros((len(self.video_infos),self.num_classes))
-                for idx,clip in enumerate(self.video_infos):
+                gt_distributions = np.zeros(
+                    (len(self.video_infos), self.num_classes))
+                for idx, clip in enumerate(self.video_infos):
                     p = np.zeros(self.num_classes)
                     for lbl in clip["orig_label"]:
                         c = self.label_to_ind(lbl)
                         p[c] += 1
                     p /= np.sum(p)
                     gt_distributions[idx] = p
-                w1dist = wasserstein_1_distance(results,gt_distributions,delta_x=4)
+                w1dist = wasserstein_1_distance(
+                    results, gt_distributions, delta_x=4)
                 eval_results['w1dist'] = w1dist
                 log_msg = f'\n Wasserstein-1 Distance:\t{w1dist}'
                 print_log(log_msg, logger=logger)
-            elif metric == 'euclidean':
-                gt_distributions = np.zeros((len(self.video_infos),self.num_classes))
-                for idx,clip in enumerate(self.video_infos):
+            elif metric == 'class_euclidean':
+                gt_distributions = np.zeros(
+                    (len(self.video_infos), self.num_classes))
+                for idx, clip in enumerate(self.video_infos):
                     p = np.zeros(self.num_classes)
                     for lbl in clip["orig_label"]:
                         c = self.label_to_ind(lbl)
                         p[c] += 1
                     p /= np.sum(p)
                     gt_distributions[idx] = p
-                eucl = euclidean_distance(results,gt_distributions,delta_x=4)
+                eucl = class_euclidean_distance(
+                    results, gt_distributions, delta_x=4)
+                eval_results['class_euclidean'] = eucl
+                log_msg = f'\n Class Euclidean Distance:\t{eucl}'
+                print_log(log_msg, logger=logger)
+            elif metric == 'euclidean':
+                gt_distributions = np.zeros(
+                    (len(self.video_infos), self.num_classes))
+                for idx, clip in enumerate(self.video_infos):
+                    p = np.zeros(self.num_classes)
+                    for lbl in clip["orig_label"]:
+                        c = self.label_to_ind(lbl)
+                        p[c] += 1
+                    p /= np.sum(p)
+                    gt_distributions[idx] = p
+                eucl = euclidean_distance(results, gt_distributions, delta_x=4)
                 eval_results['euclidean'] = eucl
                 log_msg = f'\n Euclidean Distance:\t{eucl}'
                 print_log(log_msg, logger=logger)
             elif metric == 'KL':
-                gt_distributions = np.zeros((len(self.video_infos),self.num_classes))
-                for idx,clip in enumerate(self.video_infos):
+                gt_distributions = np.zeros(
+                    (len(self.video_infos), self.num_classes))
+                for idx, clip in enumerate(self.video_infos):
                     p = np.zeros(self.num_classes)
                     for lbl in clip["orig_label"]:
                         c = self.label_to_ind(lbl)
                         p[c] += 1
                     p /= np.sum(p)
                     gt_distributions[idx] = p
-                KL_div = KL(results,gt_distributions)
+                KL_div = KL(results, gt_distributions)
                 eval_results['KL'] = KL_div
                 log_msg = f'\n KL-divergence:\t{KL_div}'
                 print_log(log_msg, logger=logger)
